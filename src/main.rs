a5cc53bea279c5474fc34e26d021386686b1f558
Nic Bonetto <nick.bonetto@gmail.com>
2018-08-08 23:10:59 -0700

    src/main.rs: Defined tokenize function.
    fn tokenize receive a string slice, and matches each character in the string
    to the Token enum. It then returns a token vector.
    
    Signed-off-by: Nic Bonetto <nick.bonetto@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -14,3 +14,24 @@ use self::Token::*;
 fn main() {
     println!("It's a Brainf*uck to C compiler!");
 }
+
+fn tokenize(input: &str) -> Vec<Token> {
+    let mut tokens = Vec::<Token>::new();
+    let mut chars = input.chars();
+
+    while let Some(c) = chars.next() {
+        match c {
+            '+' => tokens.push(Add),
+            '-' => tokens.push(Sub),
+            '>' => tokens.push(Right),
+            '<' => tokens.push(Left),
+            ',' => tokens.push(Read),
+            '.' => tokens.push(Write),
+            '[' => tokens.push(BeginLoop),
+            ']' => tokens.push(EndLoop),
+            _ => {},
+        }
+    }
+
+    tokens
+}
