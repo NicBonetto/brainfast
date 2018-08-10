522c75573ce7bf19144b7ea325ed0ad377f73a3a
Nic Bonetto <nick.bonetto@gmail.com>
2018-08-09 21:16:47 -0700

    src/compile.rs: Moved tokenize and generate functions.
    Signed-off-by: Nic Bonetto <nick.bonetto@gmail.com> #7

---
+++ src/compile.rs
@@ -0,0 +1,96 @@
+#[derive(Debug, PartialEq, Copy, Clone)]
+pub enum Token {
+  Add,        // +
+  Sub,        // -
+  Right,      // >
+  Left,       // <
+  Read,       // ,
+  Write,      // .
+  BeginLoop,  // [
+  EndLoop,    // ]
+}
+
+pub fn tokenize(input: &str) -> Vec<Token> {
+  let mut tokens = Vec::<Token>::new();
+  let mut chars = input.chars();
+
+  while let Some(c) = chars.next() {
+    match c {
+      '+' => tokens.push(Token::Add),
+      '-' => tokens.push(Token::Sub),
+      '>' => tokens.push(Token::Right),
+      '<' => tokens.push(Token::Left),
+      ',' => tokens.push(Token::Read),
+      '.' => tokens.push(Token::Write),
+      '[' => tokens.push(Token::BeginLoop),
+      ']' => tokens.push(Token::EndLoop),
+      _ => {},
+    }
+  }
+
+  tokens
+}
+
+fn indent(count: u32) -> String {
+  let mut indentation = String::new();
+
+  for _ in 0..count {
+    indentation.push_str("  ");  // 2-space indent
+  }
+  indentation
+}
+
+pub fn generate(tokens: &[Token]) -> String {
+  let mut output = String::from("int main() {\n");
+  let mut indent_count = 1u32;
+
+  for &token in tokens {
+    match token {
+      Token::Add => {
+        // Increment value at selected cell
+        output.push_str(&indent(indent_count));
+        output.push_str("++*ptr;\n");
+      }
+      Token::Sub => {
+        // Decrement value at selected cell
+        output.push_str(&indent(indent_count));
+        output.push_str("--*ptr;\n");
+      }
+      Token::Right => {
+        // Change selected cell one to the right
+        output.push_str(&indent(indent_count));
+        output.push_str("++ptr;\n");
+      }
+      Token::Left => {
+        // Change selected cell one to the left
+        output.push_str(&indent(indent_count));
+        output.push_str("--ptr;\n");
+      }
+      Token::Read => {
+        // Read a single character into the selected cell
+        output.push_str(&indent(indent_count));
+        output.push_str("*ptr=getchar();\n");
+      }
+      Token::Write => {
+        // Print character at selected cell
+        output.push_str(&indent(indent_count));
+        output.push_str("putchar(*ptr);\n");
+      }
+      Token::BeginLoop => {
+        // Begin a loop at the current cell
+        output.push_str(&indent(indent_count));
+        output.push_str("while (*ptr) {\n");
+        indent_count += 1;
+      }
+      Token::EndLoop => {
+        // End a loop
+        indent_count -= 1;
+        output.push_str(&indent(indent_count));
+        output.push_str("}\n");
+      }
+    }
+  }
+
+  output.push_str("}\n");
+  output
+}
