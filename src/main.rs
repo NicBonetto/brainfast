01ad0528dbfaaf9f90796fbdc2ec9d1d2fbb57f4
Nic Bonetto <nick.bonetto@gmail.com>
2018-08-09 21:20:25 -0700

    src/main.rs: brainfast works running main.rs.
    Signed-off-by: Nic Bonetto <nick.bonetto@gmail.com> #7

--- src/main.rs
+++ src/main.rs
@@ -1,100 +1,35 @@
-#[derive(Debug, PartialEq, Copy, Clone)]
-enum Token {
-  Add,        // +
-  Sub,        // -
-  Right,      // >
-  Left,       // <
-  Read,       // ,
-  Write,      // .
-  BeginLoop,  // [
-  EndLoop,    // ]
-}
+use std::fs::{File, OpenOptions};
+use std::io::*;
+
+mod cli;
+mod compile;
+
+use cli::app;
+use compile::{tokenize, generate};
 
 fn main() {
-  println!("It's a Brainf*ck to C compiler!");
-}
+  println!("It's a Brainf*ck to C compiler!!!\n\n");
 
-fn tokenize(input: &str) -> Vec<Token> {
-  let mut tokens = Vec::<Token>::new();
-  let mut chars = input.chars();
+  let matches = app();
+  let mut read_buffer = File::open(&matches[0])
+    .expect("Could not read Brainfuck file.");
 
-  while let Some(c) = chars.next() {
-    match c {
-      '+' => tokens.push(Token::Add),
-      '-' => tokens.push(Token::Sub),
-      '>' => tokens.push(Token::Right),
-      '<' => tokens.push(Token::Left),
-      ',' => tokens.push(Token::Read),
-      '.' => tokens.push(Token::Write),
-      '[' => tokens.push(Token::BeginLoop),
-      ']' => tokens.push(Token::EndLoop),
-      _ => {},
-    }
-  }
+  let mut contents = String::new();
+  read_buffer.read_to_string(&mut contents)
+    .expect("Something went wrong reading the Brainfuck file.");
 
-  tokens
-}
+  let tokens = tokenize(&contents);
+  let code = generate(&tokens);
 
-fn indent(count: u32) -> String {
-  let mut indentation = String::new();
+  let mut options = OpenOptions::new();
+  options.write(true);
 
-  for _ in 0..count {
-    indentation.push_str("  ");  // 2-space indent
-  }
-  indentation
-}
+  let write_file = match options.open(&matches[1]) {
+    Ok(file) => file,
+    Err(_) => panic!("No target C file found!"),
+  };
+  let mut writer = BufWriter::new(&write_file);
+  writer.write_all(code.as_bytes()).unwrap();
 
-fn generate(tokens: &[Token]) -> String {
-  let mut output = String::from("int main() {\n");
-  let mut indentCount = 1u32;
-
-  for &token in tokens {
-    match token {
-      Token::Add => {
-        // Increment value at selected cell
-        output.push_str(&indent(indentCount));
-        output.push_str("++*ptr;\n");
-      }
-      Token::Sub => {
-        // Decrement value at selected cell
-        output.push_str(&indent(indentCount));
-        output.push_str("--*ptr;\n");
-      }
-      Token::Right => {
-        // Change selected cell one to the right
-        output.push_str(&indent(indentCount));
-        output.push_str("++ptr;\n");
-      }
-      Token::Left => {
-        // Change selected cell one to the left
-        output.push_str(&indent(indentCount));
-        output.push_str("--ptr;\n");
-      }
-      Token::Read => {
-        // Read a single character into the selected cell
-        output.push_str(&indent(indentCount));
-        output.push_str("*ptr=getchar();\n");
-      }
-      Token::Write => {
-        // Print character at selected cell
-        output.push_str(&indent(indentCount));
-        output.push_str("putchar(*ptr);\n");
-      }
-      Token::BeginLoop => {
-        // Begin a loop at the current cell
-        output.push_str(&indent(indentCount));
-        output.push_str("while (*ptr) {\n");
-        indentCount += 1;
-      }
-      Token::EndLoop => {
-        // End a loop
-        indentCount -= 1;
-        output.push_str(&indent(indentCount));
-        output.push_str("}\n");
-      }
-    }
-  }
-
-  output.push_str("}\n");
-  output
+  println!("Finished compiling!");
 }
